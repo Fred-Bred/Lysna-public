46c1463f466309ee6b57230592e8f3adbc1e7d2d
Fred-Bred <frbredgaard@gmail.com>
2025-04-27 10:15:54 +0200

    Public version of lysna.language

---
+++ lysna/language.py
@@ -0,0 +1,239 @@
+class English:
+    """
+    English language class for the LYSNA application.
+    """
+
+    def __init__(self):
+        self.language = "English"
+        self.language_code = "en"
+        
+        self.scales = {
+            "org_core": "Organisational Core",
+            "team_core": "Team Core",
+            "safety": "Psychological Safety",
+            "dependability": "Professional Dependability"}
+        
+        self.labels = {
+            "count": "Count",
+            "avoidance": "Avoidance",
+            "anxiety": "Anxiety",
+            "attachment": "Attachment",
+            "Trust": "Trust",
+            "Confidence": "Confidence",
+            "Transformation": "Transformation",
+            "Dominance": "Dominance",
+            "Nurture": "Nurture",
+            }
+        
+        self.numeric_idxs = {0: ['...', '...'],
+                             1: ['...'],
+                             2: ['...']}
+        
+        self.team_filter = "Before answering the questions, please select which team you are a part of.Keep this team in mind when answering the questions. I belong to the:"
+        
+        self.reversed_items = ["...", "...", "...",
+                    "...", "...",
+                    "...", "...",
+                    "...",
+                    "...", "...",
+                    "...", "..."]
+        
+        self.reformed_items = ["...", "...", "...",
+                  "...", "...",
+                  "...", "...",
+                  "...",
+                  "...", "...",
+                  "...", "..."]
+        
+        self.attachment_idxs = ["...",
+                                "..."]
+
+        self.anxiety = ["...",
+            "...",
+            "...",
+            "...",
+            "...",
+            "..."]
+    
+        self.avoidance = ['...',
+                '...',
+                '...',
+                '...',
+                '...',
+                "...",
+                '...',
+                "..."]
+
+        self.org_core_idxs = ["...",
+                              "..."]
+        
+        self.team_core_idxs = ["...",
+                               "..."]
+        
+        self.free_form = ["...",
+                          "...",
+                          "...",]
+        
+        self.safety_idxs = ["...",
+                            "..."]
+        
+        self.dependability_idxs = ["...",
+                                   "..."]
+
+class Dutch:
+    def __init__(self):
+        self.language = "Dutch"
+        self.language_code = "nl"
+        
+        self.scales = {
+            "org_core": "Kern van de organisatie",
+            "team_core": "Team kern",
+            "safety": "Team veiligheid",
+            "dependability": "Team betrouwbaarheid"}
+        
+        self.labels = {
+            "count": "Aantal",
+            "avoidance": "Avoidance",
+            "anxiety": "Anxiety",
+            "attachment": "Attachment",
+            "Trust": "Vertrouwen",
+            "Confidence": "Assertiviteit",
+            "Transformation": "Transformatie",
+            "Dominance": "Dominantie",
+            "Nurture": "Verzorgen",
+            }
+        
+        self.numeric_idxs = {0: ['...', '...'],
+                             1: ['...'],
+                             2: ['...']}
+        
+        self.team_filter = "Before answering the questions, please select which team you are a part of.Keep this team in mind when answering the questions. I belong to the:"
+        
+        self.reversed_items = ["...", "...", "...",
+                    "...", "...",
+                    "...", "...",
+                    "...",
+                    "...", "...",
+                    "...", "..."]
+        
+        self.reformed_items = ["...", "...", "...",
+                  "...", "...",
+                  "...", "...",
+                  "...",
+                  "...", "...",
+                  "...", "..."]
+        
+        self.attachment_idxs = ["...",
+                                "..."]
+
+        self.anxiety = ["...",
+            "...",
+            "...",
+            "...",
+            "...",
+            "..."]
+    
+        self.avoidance = ['...',
+                '...',
+                '...',
+                '...',
+                '...',
+                "...",
+                '...',
+                "..."]
+
+        self.org_core_idxs = ["...",
+                              "..."]
+        
+        self.team_core_idxs = ["...",
+                               "..."]
+        
+        self.free_form = ["...",
+                          "...",
+                          "...",]
+        
+        self.safety_idxs = ["...",
+                            "..."]
+        
+        self.dependability_idxs = ["...",
+                                   "..."]
+
+class Danish:
+    """
+    Danish language class for the LYSNA application.
+    """
+
+    def __init__(self):
+        self.language = "Danish"
+        self.language_code = "da"
+        self.scales = {
+            "org_core": "Organisationens Kerne",
+            "team_core": "Teamets Kerne",
+            "safety": "Psykologisk Sikkerhed",
+            "dependability": "Professionel Pålidelighed"}
+        self.labels = {
+            "count": "Antal",
+            "avoidance": "Avoidance",
+            "anxiety": "Anxiety",
+            "attachment": "Attachment",
+            "Trust": "Tillid",
+            "Confidence": "Mod",
+            "Transformation": "Transformation",
+            "Dominance": "Dominans",
+            "Nurture": "Omsorg",
+            }
+        
+        self.numeric_idxs = {0: ['...', '...'],
+                             1: ['...'],
+                             2: ['...']}
+        
+        self.team_filter = "Before answering the questions, please select which team you are a part of.Keep this team in mind when answering the questions. I belong to the:"
+        
+        self.reversed_items = ["...", "...", "...",
+                    "...", "...",
+                    "...", "...",
+                    "...",
+                    "...", "...",
+                    "...", "..."]
+        
+        self.reformed_items = ["...", "...", "...",
+                  "...", "...",
+                  "...", "...",
+                  "...",
+                  "...", "...",
+                  "...", "..."]
+        
+        self.attachment_idxs = ["...",
+                                "..."]
+
+        self.anxiety = ["...",
+            "...",
+            "...",
+            "...",
+            "...",
+            "..."]
+    
+        self.avoidance = ['...',
+                '...',
+                '...',
+                '...',
+                '...',
+                "...",
+                '...',
+                "..."]
+
+        self.org_core_idxs = ["...",
+                              "..."]
+        
+        self.team_core_idxs = ["...",
+                               "..."]
+        
+        self.free_form = ["...",
+                          "...",
+                          "...",]
+        
+        self.safety_idxs = ["...",
+                            "..."]
+        
+        self.dependability_idxs = ["...",
+                                   "..."]
